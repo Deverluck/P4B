e5b66253fe1984cdbd5c39664558f7b01919092b
叶翀 <m15044484950@163.com>
2020-03-15 11:41:17 +0800

    Replace json-lib with Jackson (parse function completed)

--- src/verification/parser/Main.java
+++ src/verification/parser/Main.java
@@ -2,8 +2,8 @@ package verification.parser;
 
 public class Main {
 	public static void main(String args[]) {
-//		Parser myParser = new Parser("E:\\Programs\\P4-verification\\sharedir\\test\\basic--toJSON-json.json");
-		Parser myParser = new Parser("E:\\Programs\\P4-verification\\sharedir\\test\\switch-16-toJSON.json");
+		Parser myParser = new Parser("E:\\Programs\\P4-verification\\sharedir\\test\\basic--toJSON-json.json");
+//		Parser myParser = new Parser("E:\\Programs\\P4-verification\\sharedir\\test\\switch-16-toJSON.json");
 		myParser.parse();
 	}
 }
--- src/verification/parser/Parser.java
+++ src/verification/parser/Parser.java
@@ -6,7 +6,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -14,7 +20,7 @@ import net.sf.json.JSONObject;
 public class Parser {
 	private String jsonFileName;
 	static HashSet<String> types;
-	static HashMap<Integer, JSONObject> allJsonNodes;
+	static HashMap<Integer, ObjectNode> allJsonNodes;
 	
 	public Parser(String filename) {
 		jsonFileName = filename;
@@ -27,124 +33,156 @@ public class Parser {
 	}
 	
 	public void parse(String filename) {
+		ObjectMapper mapper = new ObjectMapper();
 		File file = new File(filename);
-		if(!file.exists()) {
-			System.out.println("ERROR: File doesn't exist.");
-			return;
-		}
-		System.out.println("parse:");
-		System.out.println("####################");
-		
-		// Read json file
-		BufferedReader bufferedReader;
-		try{
-			long startTime = System.currentTimeMillis();
-			
-			bufferedReader = new BufferedReader(new FileReader(file));
-			StringBuffer stringBuffer = new StringBuffer();
-			String tmpstr;
-			while((tmpstr = bufferedReader.readLine())!=null) {
-				stringBuffer.append(tmpstr);
+		try {
+			System.out.println("parse:");
+			System.out.println("####################");
+			ObjectNode rootNode = (ObjectNode)mapper.readTree(file);
+			getAllNodes(rootNode);
+			System.out.println("getAllNodes() ends");
+			System.out.println(allJsonNodes.size());
+			System.out.println(allJsonNodes.get(136));
+			HashSet<Integer> set = new HashSet<>();
+			for(Integer i : allJsonNodes.keySet()) {
+				set.add(i);
+//				System.out.println(i);
 			}
-			bufferedReader.close();
-			String res = stringBuffer.toString();
-//			JSONObject rootNode = JSONObject.fromObject(res);
-			System.out.println("json file reading ends");
-			
-			long endTime = System.currentTimeMillis(); 
-			System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
-			
-//			allJsonNodes.put(rootNode.getInt(JsonKeyName.NODE_ID), rootNode);
-//			getAllNodes(rootNode);
-//			System.out.println("getAllNodes() ends");
-//			System.out.println(allJsonNodes.size());
-//			System.out.println(allJsonNodes.get(136));
-			
-			// parse
-//			Node program = jsonParse(rootNode);
-//			System.out.println("jsonParse() ends");
-//			
-//			// for testing unhandled types
-//			String [] handledTypes = {"P4Program", "Type_Error", "Type_Extern", "Type_Header", "StructField",
-//					"Type_Bits", "Type_Name", "Path", "Type_Struct", "Type_Typedef", 
-//					"Parameter", "ParameterList", "PathExpression"};
-//			for(String str : handledTypes){
-//				types.remove(str);
-//			}
-//			System.out.println("######## Unhandled Types ########");
-//			for(String type : types) {	
-//				System.out.println(type);
-//			}
-//			System.out.println("######## Program ########");
-//			System.out.println(program.p4_to_C());
-			
-		} catch(IOException e) {
+			System.out.println(set.size());
+//			Node program = jsonParse(root)
+		}catch(JsonProcessingException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
 			e.printStackTrace();
 		}
+//		File file = new File(filename);
+//		if(!file.exists()) {
+//			System.out.println("ERROR: File doesn't exist.");
+//			return;
+//		}
+//		System.out.println("parse:");
+//		System.out.println("####################");
+//		
+//		// Read json file
+//		BufferedReader bufferedReader;
+//		try{
+//			long startTime = System.currentTimeMillis();
+//			
+//			bufferedReader = new BufferedReader(new FileReader(file));
+//			StringBuffer stringBuffer = new StringBuffer();
+//			String tmpstr;
+//			while((tmpstr = bufferedReader.readLine())!=null) {
+//				stringBuffer.append(tmpstr);
+//			}
+//			bufferedReader.close();
+//			String res = stringBuffer.toString();
+////			JSONObject rootNode = JSONObject.fromObject(res);
+//			System.out.println("json file reading ends");
+//			
+//			long endTime = System.currentTimeMillis(); 
+//			System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
+//			
+////			allJsonNodes.put(rootNode.getInt(JsonKeyName.NODE_ID), rootNode);
+////			getAllNodes(rootNode);
+////			System.out.println("getAllNodes() ends");
+////			System.out.println(allJsonNodes.size());
+////			System.out.println(allJsonNodes.get(136));
+//			
+//			// parse
+////			Node program = jsonParse(rootNode);
+////			System.out.println("jsonParse() ends");
+////			
+////			// for testing unhandled types
+////			String [] handledTypes = {"P4Program", "Type_Error", "Type_Extern", "Type_Header", "StructField",
+////					"Type_Bits", "Type_Name", "Path", "Type_Struct", "Type_Typedef", 
+////					"Parameter", "ParameterList", "PathExpression"};
+////			for(String str : handledTypes){
+////				types.remove(str);
+////			}
+////			System.out.println("######## Unhandled Types ########");
+////			for(String type : types) {	
+////				System.out.println(type);
+////			}
+////			System.out.println("######## Program ########");
+////			System.out.println(program.p4_to_C());
+//			
+//		} catch(IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 	
 	// get all nodes from JSON file and update their attributes
-	public void getAllNodes(JSONObject rootNode) {
-		if(rootNode.containsKey(JsonKeyName.NODE_ID)) {
-			int id = rootNode.getInt(JsonKeyName.NODE_ID);
+	@SuppressWarnings("deprecation")
+	public void getAllNodes(ObjectNode rootNode) {
+		if(rootNode.has(JsonKeyName.NODE_ID)) {
+			int id = rootNode.get(JsonKeyName.NODE_ID).asInt();
 			if(allJsonNodes.containsKey(id)) {
 				// update attributes
-				for(String key : (Set<String>)rootNode.keySet()) {
-					JSONObject node_at_id = allJsonNodes.get(id);
-					if(!node_at_id.containsKey(key))
+				Iterator<String> it = rootNode.fieldNames();
+				while(it.hasNext()) {
+					String key = it.next();
+					ObjectNode node_at_id = allJsonNodes.get(id);
+					if(!node_at_id.has(key))
 						node_at_id.put(key, rootNode.get(key));
 				}
+//				for(String key : (Set<String>)rootNode.fieldNames()) {
+//					JSONObject node_at_id = allJsonNodes.get(id);
+//					if(!node_at_id.containsKey(key))
+//						node_at_id.put(key, rootNode.get(key));
+//				}
 			}
 			else {
-				// add new node
+//				// add new node
 				allJsonNodes.put(id, rootNode);
 			}
 		}
 		
-		Set<String> keyset = rootNode.keySet();
-		for(String key : keyset) {
+		Iterator<String> keyset = rootNode.fieldNames();
+		while(keyset.hasNext()) {
+			String key = keyset.next();
 			Object child = rootNode.get(key);
 			if(key.equals(JsonKeyName.VEC)) {
-				JSONArray jsonArray = (JSONArray)child;
-				for(Object jo : jsonArray.toArray()) {
-					getAllNodes((JSONObject)jo);
+				ArrayNode jsonArray = (ArrayNode)child;
+				for(Object obj : jsonArray) {
+					ObjectNode childNode = (ObjectNode)obj;
+					getAllNodes(childNode);
 				}
 			}
-			else if(child instanceof JSONObject) {
-				JSONObject childNode = (JSONObject)child;
+			else if(child instanceof ObjectNode) {
+				ObjectNode childNode = (ObjectNode)child;
 				getAllNodes(childNode);
 			}
 		}
 	}
 	
 	public static Node jsonParse(JSONObject object) {
-		object = getJsonNode(object.getInt(JsonKeyName.NODE_ID));
-		String typeName = object.getString(JsonKeyName.NODE_TYPE);
-//		System.out.println(typeName);
-		try{
-			Node node;
-			if(object.has(JsonKeyName.VEC)) {
-				node = new TypeVector();
-			}
-			else {
-				types.add(typeName);
-				Class nodeClass = Class.forName("verification.parser."+typeName);
-				node = (Node)nodeClass.newInstance();
-			}
-//			System.out.println(object.getInt(JsonKeyName.NODE_ID));
-			node.parse(object);
-			return node;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		}
+//		object = getJsonNode(object.get(JsonKeyName.NODE_ID).);
+//		String typeName = object.getString(JsonKeyName.NODE_TYPE);
+////		System.out.println(typeName);
+//		try{
+//			Node node;
+//			if(object.has(JsonKeyName.VEC)) {
+//				node = new TypeVector();
+//			}
+//			else {
+//				types.add(typeName);
+//				Class nodeClass = Class.forName("verification.parser."+typeName);
+//				node = (Node)nodeClass.newInstance();
+//			}
+////			System.out.println(object.getInt(JsonKeyName.NODE_ID));
+//			node.parse(object);
+//			return node;
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IllegalAccessException e) {
+//			e.printStackTrace();
+//		} catch (InstantiationException e) {
+//			e.printStackTrace();
+//		}
 		return null;
 	}
 	
-	public static JSONObject getJsonNode(int key) {
+	public static ObjectNode getJsonNode(int key) {
 		if(allJsonNodes == null || !allJsonNodes.containsKey(key))
 			return null;
 		return allJsonNodes.get(key);
